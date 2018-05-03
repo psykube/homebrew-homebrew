4c1e23474e49b1314ff9bf52a9031533b02fdcbd
Jason Waldrip <jason@waldrip.net>
2018-05-03 12:19:06 -0600

    Update psykube.rb

--- psykube.rb
+++ psykube.rb
@@ -3,14 +3,14 @@ require "formula"
 require "language/node"
 
 class Psykube < Formula
-  LATEST_RELEASE = JSON.parse(Net::HTTP.get(URI("https://api.github.com/repos/commercialtribe/psykube/releases/latest")))
-  FALLBACK_TAG = "v1.5.2.7"
+  LATEST_RELEASE = JSON.parse(Net::HTTP.get(URI("https://api.github.com/repos/psykube/psykube/releases/latest")))
+  FALLBACK_TAG = "v1.9.3.0"
   TAG = LATEST_RELEASE["tag_name"] || FALLBACK_TAG
 
   version TAG.sub /^v/, ''
-  homepage 'https://github.com/commercialtribe/psykube'
-  head 'https://github.com/commercialtribe/psykube.git', branch: 'master'
-  url 'https://github.com/commercialtribe/psykube.git', using: :git, tag: TAG
+  homepage 'https://github.com/psykube/psykube'
+  head 'https://github.com/psykube/psykube.git', branch: 'master'
+  url 'https://github.com/psykube/psykube.git', using: :git, tag: TAG
 
   depends_on 'crystal-lang' => :build
   depends_on 'openssl' => :build
